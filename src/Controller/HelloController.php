93a5fcbb4073c7e8f0024581b97195f013f6a758
Adrien Roches <adrien@roc-it.tech>
2023-10-10 12:21:25 +0200

    01-controller : Display simple Hello

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,17 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-    #[Route('/hello', name: 'app_hello')]
-    public function index(): JsonResponse
+    #[Route('/hello', name: 'app_hello', methods: ['GET'])]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HelloController.php',
-        ]);
+        return new Response(
+            content: <<<"HTML"
+            <body>
+                Hello !
+            </body>
+            HTML
+        );
     }
 }
