00447de4d39e9aaf111dcc19efe3c9cd76879b53
Adrien Roches <adrien@roc-it.tech>
2023-10-12 14:41:32 +0200

    08-console : Add enum for Rating in Entity

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Model\Rating;
 use App\Repository\MovieRepository;
 use App\Validator\Constraints\MoviePosterExists;
 use App\Validator\Constraints\PosterValid;
@@ -54,8 +55,8 @@ class Movie
     #[Assert\Count(min: 1)]
     private Collection $genres;
 
-    #[ORM\Column(length: 6)]
-    private ?string $rated = null;
+    #[ORM\Column(length: 6, enumType: Rating::class, options: ['default' => Rating::GeneralAudiences->value])]
+    private Rating $rated = Rating::GeneralAudiences;
 
     public function __construct()
     {
@@ -151,12 +152,12 @@ class Movie
         return $this;
     }
 
-    public function getRated(): ?string
+    public function getRated(): Rating
     {
         return $this->rated;
     }
 
-    public function setRated(string $rated): static
+    public function setRated(Rating $rated): static
     {
         $this->rated = $rated;
 
